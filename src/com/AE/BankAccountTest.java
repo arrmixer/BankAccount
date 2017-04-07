64a272e187d5243f716e58dbd8d519a92e457900
Angel Rodriguez <arrmixer@gmail.com>
2017-04-07 17:15:13 -0400

    setting up a Parameter class

--- src/com/AE/BankAccountTest.java
+++ src/com/AE/BankAccountTest.java
@@ -37,8 +37,14 @@ public class BankAccountTest {
 
     @org.junit.Test(expected = IllegalArgumentException.class)
     public void withdraw_notBranch() throws Exception {
-        double balance = account.withdraw(600.00, false);
-        assertEquals(400.00, balance, 0);
+        account.withdraw(600.00, false);
+// JUnit 3 implementation
+        //        try{
+//            account.withdraw(600.00, true);
+//            fail("Should have thrown an IllegalArguementException");
+//        }catch(IllegalArguementException e){
+//
+//        }
     }
 
 
